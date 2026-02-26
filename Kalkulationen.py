377ca9ec1f8b0f5d1aa28b2d0b4c1fc66cb3a73a
Ascona89 <fyhchsys4g@privaterelay.appleid.com>
2026-02-26 08:12:16 +0100

    Update Kalkulationen.py

--- Kalkulationen.py
+++ Kalkulationen.py
@@ -237,7 +237,7 @@ def show_contractnumbers():
     })
 
     df_hw = pd.DataFrame({
-        "Produkt": ["Ordermanager", "POS", "Cash Drawer", "Printer", "Additional Display", "PAX"],
+        "Produkt": ["Ordermanager", "POS inkl 1 Printer", "Cash Drawer", "Extra Printer", "Additional Display", "PAX"],
         "List_OTF": [299, 1699, 149, 199, 100, 299],
         "Min_OTF": [135, 350, 50, 99, 100, 225],
         "List_MRR": [0] * 6,
@@ -464,7 +464,6 @@ GMB:
 Discount: 
 """
     st.text_area("📄 Generierter Vertrags-Text", contract_text, height=420)
-
 # =====================================================
 # 💰 Pricing
 # =====================================================
